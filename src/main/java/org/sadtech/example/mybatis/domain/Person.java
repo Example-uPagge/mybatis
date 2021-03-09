05e9448ab2fe7694c367083d613e6564d7e1cf6c
uPagge <git@upagge.ru>
2021-03-09 20:54:59 +0300

    Create domain Person and Table

---
+++ src/main/java/org/sadtech/example/mybatis/domain/Person.java
@@ -0,0 +1,17 @@
+package org.sadtech.example.mybatis.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author upagge 09.03.2021
+ */
+@Getter
+@Setter
+public class Person {
+
+    private Long id;
+    private String name;
+    private Long telegramId;
+
+}
